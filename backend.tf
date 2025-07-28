e16adb615ba3186fc010456a63131b3ee3bfd905
HajeeraKhatun <47415269+HajeeraKhatun@users.noreply.github.com>
2025-07-28 21:39:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "azurebackendstorageabhi"
+    storage_account_name = "azurebackendstoragehajju"
     container_name = "backend"
     key = "terraform.tfstate"
-    access_key = ""
+    access_key = "Go8d5nlqb/HebiNyR/HwIWyP1gcrX2nFR43pNAEyQ5bfsuZXq+mt8WXPrsmWOghO0dAZeIN2W4Yh+ASt2i5nXw=="
   }
 }
