176124df303b917e611ba895e4b89ab58ec0d5ea
HajeeraKhatun <47415269+HajeeraKhatun@users.noreply.github.com>
2025-07-29 03:55:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,15 +49,12 @@ resource "azurerm_linux_virtual_machine" "example" {
   location                        = azurerm_resource_group.example.location
   size                            = "Standard_F2"
   admin_username                  = "adminuser"
-  disable_password_authentication = true
+  admin_password                  = "Sungalaxy@123"
+  disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   os_disk {
     caching              = "ReadWrite"
